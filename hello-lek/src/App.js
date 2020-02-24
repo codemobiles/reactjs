45ebfd769626321d74fb0af72dcaef71eea4fc94
codemobiles <chaiyasit.t@gmail.com>
2020-02-24 11:21:03 +0700

    123

--- hello-lek/src/App.js
+++ hello-lek/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+export default function App() {
+
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log("Initial")    
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <br/>
+      {/* JSX */}
+      <button onClick={()=>setCount(count+1)}>ADD</button>
     </div>
-  );
+  )
 }
-
-export default App;
