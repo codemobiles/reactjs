bb26a9126c327dc712703f325c1533ab37332313
codemobiles <chaiyasit.t@gmail.com>
2020-02-24 15:49:00 +0700

    123

--- lekpos/src/components/pages/LoginPage/LoginPage.js
+++ lekpos/src/components/pages/LoginPage/LoginPage.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Formik } from "formik";
 
+const showForm = (props) => {
+  return (
+    <form>
+      <input type="text" name="username" placeholder="Username" value={props.values.username} />
+      <br />
+      <input type="text" name="password" placeholder="Password" value={props.values.password}/>
+      <br />
+      <button>Submit</button>
+    </form>
+  );
+};
+
 const LoginPage = () => {
   return (
     <div>
-      <Formik>
-        <form>
-          <input type="text" name="username" placeholder="Username" />
-          <br />
-          <input type="text" name="password" placeholder="Password" />
-          <br />
-          <button>Submit</button>
-        </form>
+      <Formik initialValues={{username: "lek", password: "1234"}}>
+        {props=>showForm(props)}
       </Formik>
     </div>
   );
