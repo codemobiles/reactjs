bb0b177294a59171ab63f632f2918263125fdfa8
codemobiles <chaiyasit.t@gmail.com>
2020-02-24 14:17:09 +0700

    123

--- lekpos/src/components/layouts/Header.js
+++ lekpos/src/components/layouts/Header.js
@@ -50,7 +50,7 @@ export default function PermanentDrawerLeft() {
             Permanent drawer
           </Typography>
         </Toolbar>
-      </AppBar>
+      </AppBar>    
     </div>
   );
 }
