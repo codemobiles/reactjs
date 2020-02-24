e3eaf8bf35a50763565290da4aed35c99b67b328
codemobiles <chaiyasit.t@gmail.com>
2020-02-24 13:54:56 +0700

    123

--- lekpos/src/App.js
+++ lekpos/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/layouts/Header'
+import Menu from './components/layouts/Menu'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Menu/>
     </div>
-  );
+  )
 }
-
-export default App;
