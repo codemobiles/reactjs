01a060cb1b90139ba37f9cbc8283dac31cb8c8b6
codemobiles <chaiyasit.t@gmail.com>
2020-02-26 11:14:18 +0700

    123

--- lekpos/backend/api_auth.js
+++ lekpos/backend/api_auth.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("./models/user_schema");
+var bcrypt = require("bcryptjs");
+const jwt = require("./jwt");
+const randtoken = require("rand-token"); // yarn add rand-token
 
 router.post("/login", (req, res) => {
   res.json({ result: "login", message: req.body });
 });
 
+
 router.post("/register", async (req, res) => {
-  let doc = await Users.create(req.body);
-  res.json({ result: "ok", doc });
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 8);
+    await Users.create(req.body);
+    res.json({ result: "ok", message: "Register successfully" });
+  } catch (err) {
+    res.json({ result: "nok", message: err.errmsg });
+  }
 });
+
 
 module.exports = router;
