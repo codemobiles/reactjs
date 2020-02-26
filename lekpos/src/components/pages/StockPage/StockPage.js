a7119c5fbd66b19c04360472a9438bf4fc0cbc4c
codemobiles <chaiyasit.t@gmail.com>
2020-02-26 15:47:12 +0700

    123

--- lekpos/src/components/pages/StockPage/StockPage.js
+++ lekpos/src/components/pages/StockPage/StockPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import MaterialTable, { MTableToolbar } from "material-table";
 import * as stockActions from "./../../../actions/stock.action";
 import { useDispatch, useSelector } from "react-redux";
+import { imageUrl } from "./../../../constants";
 
 const StockPage = () => {
   const dispatch = useDispatch();
@@ -9,14 +10,11 @@ const StockPage = () => {
 
   React.useEffect(() => {
     // Intailization
-    dispatch(stockActions.getProducts())
-
+    dispatch(stockActions.getProducts());
   }, []);
 
   return (
     <div style={{ marginTop: 100 }}>
-   
-
       <MaterialTable
         title="Simple Action Preview"
         data={stockReducer.result ? stockReducer.result : []}
@@ -27,7 +25,21 @@ const StockPage = () => {
               return <h3>{item.name}</h3>;
             }
           },
-          { title: "Price", field: "price" }
+          {
+            title: "Image",
+            render: item => {
+              return (
+                <img
+                  src={`${imageUrl}/images/${
+                    item.image
+                  }?dummy=${Math.random()}`}
+                  style={{ width: 70, height: 70, borderRadius: "5%" }}
+                />
+              );
+            }
+          },
+          { title: "Price", field: "price" },
+          { title: "Stock", field: "stock" }
         ]}
       />
     </div>
