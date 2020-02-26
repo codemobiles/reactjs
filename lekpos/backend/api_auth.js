da1e308ab61db5369edb2be8d641325ae641899c
codemobiles <chaiyasit.t@gmail.com>
2020-02-26 11:02:37 +0700

    123

--- lekpos/backend/api_auth.js
+++ lekpos/backend/api_auth.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const Users = require("./models/user_schema");
 
 router.post("/login", (req, res) => {
   res.json({ result: "login", message: req.body });
+});
+
+router.post("/register", async (req, res) => {
+  let doc = await Users.create(req.body);
+  res.json({ result: "ok", doc });
 });
 
 module.exports = router;
