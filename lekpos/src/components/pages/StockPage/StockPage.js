152288dd3527eaf85f7448d59681791c58dd305f
codemobiles <chaiyasit.t@gmail.com>
2020-02-26 15:26:46 +0700

    123

--- lekpos/src/components/pages/StockPage/StockPage.js
+++ lekpos/src/components/pages/StockPage/StockPage.js
@@ -12,31 +12,19 @@ const StockPage = () => {
 
       <MaterialTable
         title="Simple Action Preview"
-        columns={[
-          { title: "Name", field: "name" },
-          { title: "Surname", field: "surname" },
-          { title: "Birth Year", field: "birthYear", type: "numeric" },
-          {
-            title: "Birth Place",
-            field: "birthCity",
-            lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-          }
-        ]}
         data={[
-          { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-          {
-            name: "Zerya Betül",
-            surname: "Baran",
-            birthYear: 2017,
-            birthCity: 34
-          }
+          { name: "Angular", price: 100 },
+          { name: "Vue", price: 10 },
+          { name: "React", price: 5 }
         ]}
-        actions={[
+        columns={[
           {
-            icon: "save",
-            tooltip: "Save User",
-            onClick: (event, rowData) => alert("You saved " + rowData.name)
-          }
+            title: "Name",
+            render: item => {
+              return <h3>{item.name}</h3>;
+            }
+          },
+          { title: "Price", field: "price" }
         ]}
       />
     </div>
