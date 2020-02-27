cdcfd41b4a140fe367fe3bca796fdd63370c61dd
codemobiles <chaiyasit.t@gmail.com>
2020-02-27 09:44:10 +0700

    123

--- lekpos/src/components/fragments/StockCard/StockCard.js
+++ lekpos/src/components/fragments/StockCard/StockCard.js
@@ -7,7 +7,12 @@ export default function StockCard() {
     <Card>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <h3>Title</h3>
+          <Typography variant="h4" color="textPrimary">
+            Title
+          </Typography>
+          <Typography variant="h5" color="textSecondary">
+            Subtitle
+          </Typography>
         </Grid>
 
         <Grid item xs={6}>
