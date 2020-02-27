f3f9ccc0cfe7d209731d7dfd1327639f477b1486
codemobiles <chaiyasit.t@gmail.com>
2020-02-27 15:56:39 +0700

    123

--- lekpos/backend/api_transaction.js
+++ lekpos/backend/api_transaction.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Trans = require("./models/trans_schema");
+const jwt = require("./jwt");
 
-router.get("/transaction", (req, res) => {
-  res.json({ result: "transaction" });
+router.post("/transaction", jwt.verify, async (req, res) => {
+  try {    
+    req.body.staff_id = req.userId
+    let doc = await Trans.create(req.body);
+    res.json({ result: "ok", message: doc });
+  } catch (error) {
+    res.json({ result: "nok", message: error });
+  }
 });
 
 module.exports = router;
