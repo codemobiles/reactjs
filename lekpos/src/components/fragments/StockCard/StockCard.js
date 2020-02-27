c4e7ef89a54c2abd975f69a523ea3c91062eeb8b
codemobiles <chaiyasit.t@gmail.com>
2020-02-27 09:42:53 +0700

    123

--- lekpos/src/components/fragments/StockCard/StockCard.js
+++ lekpos/src/components/fragments/StockCard/StockCard.js
@@ -4,8 +4,16 @@ import { Typography, Grid } from "@material-ui/core";
 
 export default function StockCard() {
   return (
-    <div>
-      <h3>StockCard</h3>
-    </div>
+    <Card>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <h3>Title</h3>
+        </Grid>
+
+        <Grid item xs={6}>
+          <h3>Title</h3>
+        </Grid>
+      </Grid>
+    </Card>
   );
 }
